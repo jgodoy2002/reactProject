dffca848b8a3153590910c1bbaed00c3ca191e23
jgodoy2002 <j.godoy02@unitec.edu>
2025-03-18 17:23:31 -0600

    Nuevos cambios al css

--- reactProject/src/components/btn.jsx
+++ reactProject/src/components/btn.jsx
@@ -1,11 +1,16 @@
-
+import styles from './btn.module.css'
 const Button = ({name,setCount,count}) => {
 
     return(
         <>
-            <div onClick={()=>setCount((count= count * 2))}>
-                <h1>{name}</h1>
+            <div 
+            onClick={()=>setCount((count= count * 2))} 
+            >
+                <h1 className={styles.display}>
+                    {name}
+                </h1>
             </div>
+
         </>
     )
 
